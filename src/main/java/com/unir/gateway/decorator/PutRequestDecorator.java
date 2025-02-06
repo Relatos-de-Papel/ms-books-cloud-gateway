a2552792e3ee860081159fb02db3af7d75a5b53b
Elena Parra Cedeño <ele091321@gmail.com>
2025-02-05 23:27:12 -0500

    Se crea el decorador de PUT

--- src/main/java/com/unir/gateway/decorator/PutRequestDecorator.java
+++ src/main/java/com/unir/gateway/decorator/PutRequestDecorator.java
@@ -17,41 +17,24 @@ import reactor.core.publisher.Flux;
 
 import java.net.URI;
 
-/**
- * This class is a decorator for the GatewayRequest object for POST requests.
- * It extends the ServerHttpRequestDecorator class and overrides its methods to modify the request.
- * It uses the ObjectMapper to convert the body of the GatewayRequest object into bytes.
- */
 @Slf4j
-public class PostRequestDecorator extends ServerHttpRequestDecorator {
+public class PutRequestDecorator extends ServerHttpRequestDecorator {
 
     private final GatewayRequest gatewayRequest;
     private final ObjectMapper objectMapper;
 
-    public PostRequestDecorator(GatewayRequest gatewayRequest, ObjectMapper objectMapper) {
+    public PutRequestDecorator(GatewayRequest gatewayRequest, ObjectMapper objectMapper) {
         super(gatewayRequest.getExchange().getRequest());
         this.gatewayRequest = gatewayRequest;
         this.objectMapper = objectMapper;
     }
 
-    /**
-     * This method overrides the getMethod method of the ServerHttpRequestDecorator class.
-     * It returns the HTTP method of the request, which is POST.
-     *
-     * @return the HTTP method of the request
-     */
     @Override
     @NonNull
     public HttpMethod getMethod() {
-        return HttpMethod.POST;
+        return HttpMethod.PUT;
     }
 
-    /**
-     * This method overrides the getURI method of the ServerHttpRequestDecorator class.
-     * It returns the URI of the request.
-     *
-     * @return the URI of the request
-     */
     @Override
     @NonNull
     public URI getURI() {
